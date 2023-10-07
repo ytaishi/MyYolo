b99f424858deec9c915eeef94c9dffcbe1a254ff
ytaishi <taishi5127@gmail.com>
2023-10-07 11:55:37 +0900

    windowに4分割で表示
    todo
    * 白線検出

--- src/main.py
+++ src/main.py
@@ -50,8 +50,10 @@ class ObjectDetector:
 
     # 物体の検出
     def detect_objects(self, img, depth_image):
-        result = self.model(img)
-        result.render()
+        img_copy = img.copy()  # imgのコピーを作成
+        result = self.model(img_copy)  # コピーに対してYoloの検出を行う
+        rendered_image = result.render()  # Yoloの検出結果ありの画像を取得
+
         detected_objects = []
         for detection in result.xyxy[0]:
             if (detection[2] - detection[0]) * (
@@ -63,7 +65,7 @@ class ObjectDetector:
             y_center = (detection[1] + detection[3]) / 2
             distance = self.calculate_distance(depth_image, x_center, y_center)
             detected_objects.append((label, distance))
-        return detected_objects
+        return detected_objects, rendered_image[0]  # 検出結果と画像の両方を返す
 
 
 # 検出データの整理と送信
@@ -101,14 +103,24 @@ class DisplayManager:
     def __init__(self):
         pass
 
-    # 画像の表示
-    def display(self, color_image, depth_image):
+    # 4分割の画像の表示
+    def display_quadrants(self, raw_image, yolo_image, depth_image):
+        # 生の映像を白黒にする
+        grayscale_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
+        grayscale_image = cv2.cvtColor(grayscale_image, cv2.COLOR_GRAY2BGR)
+
+        # Depthの画像をカラーマップに変換
         depth_colormap = cv2.applyColorMap(
             cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET
         )
-        images = np.hstack((color_image, depth_colormap))
-        cv2.namedWindow("RealSense", cv2.WINDOW_AUTOSIZE)
-        cv2.imshow("RealSense", images)
+
+        # 4つの画像を上下左右に結合
+        top_images = np.hstack((raw_image, yolo_image))
+        bottom_images = np.hstack((depth_colormap, grayscale_image))
+        combined = np.vstack((top_images, bottom_images))
+
+        cv2.namedWindow("Quadrants", cv2.WINDOW_NORMAL)
+        cv2.imshow("Quadrants", combined)
         cv2.waitKey(1)
 
 
@@ -132,15 +144,18 @@ def main():
             if not depth_frame or not color_frame:
                 continue
             depth_image = np.asanyarray(depth_frame.get_data())
-            color_image = np.asanyarray(color_frame.get_data())
+            raw_color_image = np.asanyarray(color_frame.get_data())  # 生の映像を保存
             hole_filling = rs.hole_filling_filter(1)
             depth_frame = hole_filling.process(depth_frame)
 
-            detected_objects = object_detector.detect_objects(color_image, depth_image)
+            detected_objects, yolo_image = object_detector.detect_objects(
+                raw_color_image, depth_image
+            )  # 変更された部分
+
             detected_data = data_organizer.organize_data(detected_objects)
             data_organizer.send_data(detected_data)
 
-            display_manager.display(color_image, depth_image)
+            display_manager.display_quadrants(raw_color_image, yolo_image, depth_image)
     finally:
         realsense_manager.stop_streaming()
 
